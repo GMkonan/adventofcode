a7b8a8efdf1e77d440d3fe5e39cfb75c65522ce2
GMkonan <guilhemont@hotmail.com>
2024-12-02 23:37:22 -0300

    Remove main comments

--- 2024/go/main.go
+++ 2024/go/main.go
@@ -9,8 +9,6 @@ func main() {
 	days := map[int]func() (int64, int64){
 		1: day.One,
 		2: day.Two,
-		// "2A": day.TwoA,
-		// "2B": day.TwoB,
 	}
 
 	oneA, oneB := days[1]()
@@ -19,11 +17,4 @@ func main() {
 	twoA, twoB := days[2]()
 	fmt.Println(twoA)
 	fmt.Println(twoB)
-	// val1B := days["1B"]()
-	// val2A := days["2A"]()
-	// val2B := days["2B"]()
-	// fmt.Println(val1A)
-	// fmt.Println(val1B)
-	// fmt.Println(val2A)
-	// fmt.Println(val2B)
 }
